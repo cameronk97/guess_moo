b6f0b616c855ff95149fa551e8e19e2c11433f0d
cameronk97 <>
2023-05-02 21:53:36 +0000

    Assign ASCII art from pyfiglet to WELCOME_LOGO constant and center

--- game_art.py
+++ game_art.py
@@ -6,5 +6,12 @@ from extras import *
 colorama.init(autoreset=True)
 
 WELCOME_LOGO = """
+                    WELCOME TO
+  ____ _   _ _____ ____ ____    __  __  ___   ___  
+ / ___| | | | ____/ ___/ ___|  |  \/  |/ _ \ / _ \ 
+| |  _| | | |  _| \___ \___ \  | |\/| | | | | | | |
+| |_| | |_| | |___ ___) |__) | | |  | | |_| | |_| |
+ \____|\___/|_____|____/____/  |_|  |_|\___/ \___/ 
 
-"""
+           THE FARM ANIMAL GUESSING GAME
+""".center(51)
