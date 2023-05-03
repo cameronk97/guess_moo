cbd35ffd769550d24d6d8e333c613bebf1fd220e
cameronk97 <>
2023-05-03 22:19:10 +0000

    Add validation loop for player_answer in game function

--- run.py
+++ run.py
@@ -198,4 +198,7 @@ def game(animals_list, key="probability"):
     for i in range(20):
         question_number = i + 1
         print(f"{colorama.Fore.GREEN}QUESTION {question_number}:")
-        player_answer = ask_question(asked_traits, possible_animals)
+        while True:
+            player_answer = ask_question(asked_traits, possible_animals)
+            if player_answer in [True, False, None]:
+                break
