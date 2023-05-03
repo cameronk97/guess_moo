ba64b647588dcbfc27273e06a319f64a8fef35f0
cameronk97 <>
2023-05-03 15:37:24 +0000

    Edit typewriter function in extras.py

--- extras.py
+++ extras.py
@@ -1,17 +1,6 @@
 import time
 import sys
 
-<<<<<<< HEAD
-def typewriter(write):
-    """
-    Creates a typewriter effect for output.
-    Code for the typewriter effect was found here:
-    https://replit.com/talk/learn/Typewriter-effect-Python/139897
-    """
-    for i in write:
-        sys.stdout.write(i)
-        sys.stdout.flush()
-=======
 def typewriter(string):
     """
     Creates a typewriter effect for output.
@@ -20,5 +9,4 @@ def typewriter(string):
     """
     for char in string:
         print(char, end='', flush=True)
->>>>>>> ebf92c8 (Define typewriter function in extras.py)
         time.sleep(.05)
