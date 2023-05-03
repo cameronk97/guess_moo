c900357cd772ca54f5ab3a9e147305b245803c34
cameronk97 <>
2023-05-03 15:29:52 +0000

    Merge accidental duplicate branch into main

--- extras.py
+++ extras.py
@@ -1,6 +1,7 @@
 import time
 import sys
 
+<<<<<<< HEAD
 def typewriter(write):
     """
     Creates a typewriter effect for output.
@@ -10,4 +11,14 @@ def typewriter(write):
     for i in write:
         sys.stdout.write(i)
         sys.stdout.flush()
+=======
+def typewriter(string):
+    """
+    Creates a typewriter effect for output.
+    Code for the typewriter effect was found here:
+    https://stackoverflow.com/questions/42940747/python-simple-typewriter-effect
+    """
+    for char in string:
+        print(char, end='', flush=True)
+>>>>>>> ebf92c8 (Define typewriter function in extras.py)
         time.sleep(.05)
