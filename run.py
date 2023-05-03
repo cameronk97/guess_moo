d287a2659a79b4b5c7c8c66e440aa577d2ff3dbe
cameronk97 <>
2023-05-03 01:17:57 +0000

    Define start_screen function to print game logo and welcome message

--- run.py
+++ run.py
@@ -10,3 +10,14 @@ from animals import animals_list
 colorama.init(autoreset=True)
 
 VALID_ANSWERS = ["yes", "y", "no", "n", "i don't know", "idk", "i dont know"]
+
+def start_screen(WELCOME_LOGO):
+    """
+    Print game logo
+    imported locally from game_art.py
+    and welcome message with typewriter effect
+    prompt player to press enter to start
+    """
+    print(f"{colorama.Fore.GREEN}{WELCOME_LOGO}\n")
+    typewriter(f"{colorama.Fore.CYAN}LET'S SEE IF I CAN GUESS THE FARM ANIMAL YOU'RE THINKING OF\n")
+    input(f"{colorama.Fore.WHITE}PRESS ENTER TO START\n>>> ")
