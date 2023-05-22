fb1557850219bc9c262ddb86ad7dc1f9a0a2c7af
cameronk97 <>
2023-05-22 09:23:37 +0000

    Remove trailing whitespace in game_art.py to fix python linter warnings

--- game_art.py
+++ game_art.py
@@ -2,14 +2,13 @@ import colorama
 from colorama import Fore, Style
 
 colorama.init(autoreset=True)
-
 WELCOME_LOGO = r"""
                     WELCOME TO
-  ____ _   _ _____ ____ ____    __  __  ___   ___  
- / ___| | | | ____/ ___/ ___|  |  \/  |/ _ \ / _ \ 
+  ____ _   _ _____ ____ ____    __  __  ___   ___
+ / ___| | | | ____/ ___/ ___|  |  \/  |/ _ \ / _ \
 | |  _| | | |  _| \___ \___ \  | |\/| | | | | | | |
 | |_| | |_| | |___ ___) |__) | | |  | | |_| | |_| |
- \____|\___/|_____|____/____/  |_|  |_|\___/ \___/ 
+ \____|\___/|_____|____/____/  |_|  |_|\___/ \___/
 
            THE FARM ANIMAL GUESSING GAME
 """
@@ -24,25 +23,25 @@ PLEASE SELECT AN OPTION:
 """
 
 game_info = """
-    ==================================================================    
-   ||                                                                ||   
-   ||                                                                ||   
-   ||                       G A M E   I N F O                        ||   
-   ||                     ---------------------                      ||   
-   ||                                                                ||   
-   ||                                                                ||   
-   ||             * The player thinks of a farm animal               ||   
-   ||                                                                ||   
-   ||                                                                ||   
-   ||        * The computer asks up to 20 yes or no questions        ||   
-   ||                                                                ||   
-   ||                                                                ||   
-   ||            * The computer attempts to guess which              ||   
-   ||              farm animal the player is thinking of             ||   
-   ||                                                                ||   
-   ||                                                                ||   
-   ||                                                                ||   
-    ==================================================================    
+    ==================================================================
+   ||                                                                ||
+   ||                                                                ||
+   ||                       G A M E   I N F O                        ||
+   ||                     ---------------------                      ||
+   ||                                                                ||
+   ||                                                                ||
+   ||             * The player thinks of a farm animal               ||
+   ||                                                                ||
+   ||                                                                ||
+   ||        * The computer asks up to 20 yes or no questions        ||
+   ||                                                                ||
+   ||                                                                ||
+   ||            * The computer attempts to guess which              ||
+   ||              farm animal the player is thinking of             ||
+   ||                                                                ||
+   ||                                                                ||
+   ||                                                                ||
+    ==================================================================
 """
 BORDER = (f"{Fore.MAGENTA}{Style.BRIGHT}\n"
           f"* * * * * * * * * * * * * * * * * * * * "
